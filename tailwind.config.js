f63a0f93de1cb9e0d44cec71923efe1a6d0fc9e0
Fede <fededoria1946@gmail.com>
2026-07-01 02:24:40 +0000

    Laracon net finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
         "./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}"
     ],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                pulse: {
+                    '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+                    '50%': { transform: 'scale(1.2)', opacity: '0.7' },
+                },
+            },
+            animation: {
+                'pulse-custom': 'pulse 2s ease-in-out infinite',
+            },
+        },
     },
+
     plugins: [],
 }
